cc2f903405596392acced09f27540f3b77c9d993
Mason Garrison <garrissm@wfu.edu>
2023-08-21 12:25:52 -0400

    longer draft

--- R/BGmisc-package.R
+++ R/BGmisc-package.R
@@ -2,11 +2,13 @@
 #'
 #' @description This collection contains functions for behavior genetic
 #'   modeling. These functions include model identification, calculating
-#'   relatedness, and various others (e.g.  Hunter, Garrison, et al, 2019
-#'   <doi:10.1007/s10519-019-09973-8>).
+#'   relatedness, and various others (e.g.  Hunter, Garrison, et al, 2021
+#'   <doi:10.1007/s10519-019-09973-8>). See the package vignette for details: \cr\cr
 #'
-#' @author S. Mason Garrison, Michael D. Hunter, S. Alexandra Burt, and Jonathan
-#'   D. Trattner
+#' @author S. Mason Garrison, Michael D. Hunter, Jonathan D. Trattner, and 
+#' S. Alexandra Burt
+#' 
+#' @docType package  
 #' @aliases BGmisc-package
 #' @name    BGmisc_package
 #' @rdname  BGmisc_package
