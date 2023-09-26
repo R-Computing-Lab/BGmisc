3f7c4a899fd3ba22b67d2f59e417f79f21082661
Mason Garrison <garrissm@wfu.edu>
2023-09-26 17:20:37 -0400

    create validation function

---
+++ R/validatePedigree.R
@@ -0,0 +1,17 @@
+#' Validate Pedigrees
+#'
+#' This function validates pedigrees based on several criteria
+
+#' @param ped A pedigree object
+#' @param verbose A logical indicating whether to print progress messages
+#' @param ... Additional arguments to be passed to \code{\link{validatePedigree}}
+#' @return A logical indicating whether the pedigree is valid and a list of warnings and ids of potentially invalid relationships
+#' @export
+#' 
+
+validatePedigree() <- function(ped,
+                        verbose = FALSE){
+
+
+                            
+                        }
