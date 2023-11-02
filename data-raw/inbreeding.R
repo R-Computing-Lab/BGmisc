85dfc0c4e62e780181c8c782865a8d43750ae654
Mason Garrison <garrissm@wfu.edu>
2023-11-02 12:16:10 -0400

    Update validatePedigree.R

---
+++ data-raw/inbreeding.R
@@ -0,0 +1,13 @@
+
+#devtools::install_github("R-Computing-Lab/BGmisc")
+library(tidyverse)
+library(usethis)
+
+raw <- read.csv("data-raw/inbreeding.csv", header = TRUE, sep = ",")
+
+inbreeding <- raw
+##
+# data processing
+
+write.csv(inbreeding, "data-raw/inbreeding.csv", row.names = FALSE)
+usethis::use_data(inbreeding, overwrite = TRUE, compress = 'xz')
