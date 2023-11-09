11d4d55ed10e3086fc12809e00bd2f19dbe4ca73
mhunter1 <mhunter.ou@gmail.com>
2023-11-08 23:34:31 -0500

    Add full maternal/paternal line functions

--- R/family.R
+++ R/family.R
@@ -21,8 +21,15 @@
 #' @export
 #'
 ped2fam <- function(ped, personID = "ID", momID = "momID", dadID = "dadID", famID = "famID") {
+  # Call to wrapper function
+  .ped2id(ped = ped, personID = personID, momID = momID, dadID = dadID, famID = famID, type = 'parents')
+}
+
+.ped2id <- function(ped,
+                    personID = "ID", momID = "momID", dadID = "dadID",
+                    famID = "famID", type) {
   # Turn pedigree into family
-  pg <- ped2graph(ped = ped, personID = personID, momID = momID, dadID = dadID, famID = famID)
+  pg <- ped2graph(ped = ped, personID = personID, momID = momID, dadID = dadID, adjacent = type)
 
   # Find weakly connected components of graph
   wcc <- igraph::components(pg)
@@ -33,20 +40,28 @@ ped2fam <- function(ped, personID = "ID", momID = "momID", dadID = "dadID", famI
   )
   names(fam) <- c(personID, famID)
   ped2 <- merge(fam, ped,
-    by = personID, all.x = FALSE, all.y = TRUE
+                by = personID, all.x = FALSE, all.y = TRUE
   )
 
   return(ped2)
 }
 
+
 #' Turn a pedigree into a graph
 #' @param ped a pedigree dataset.  Needs ID, momID, and dadID columns
 #' @inheritParams ped2fam
 #' @param directed Logical scalar. Default is TRUE. Indicates whether or not to create a directed graph.
+#' @param adjacent Character.  Relationship that defines adjacency in the graph: parents, mothers, or fathers
 #' @details
 #' The general idea of this function is to represent a pedigree as a graph using the igraph package.
 #'
 #' Once in graph form, several common pedigree tasks become much simpler.
+#'
+#' The \code{adjacent} argument allows for different kinds of graph structures.
+#' When using \code{parents} for adjacency, the graph shows all parent-child relationships.
+#' When using \code{mother} for adjacency, the graph only shows mother-child relationships.
+#' Similarly when using \code{father} for adjacency, only father-child relationships appear in the graph.
+#' Extended families can be constructed from the parent graph, maternal lines from the mothers graph, and paternal lines from the fathers graph.
 #'
 #' @returns
 #' A graph
@@ -57,20 +72,60 @@ ped2graph <- function(ped,
                       personID = "ID",
                       momID = "momID",
                       dadID = "dadID",
-                      famID = "famID",
-                      directed = TRUE) {
+                      directed = TRUE,
+                      adjacent = c('parents', 'mothers', 'fathers')) {
+  # Check ped/data.fram
   if (!inherits(ped, "data.frame")) stop("ped should be a data.frame or inherit to a data.frame")
-  if (!all(c() %in% names(ped))) stop("'personID', 'momID', and 'dadID' were not all found in your pedigree.\nMake sure you have the variable names correct.")
+  # Handle adjacent argument
+  adjacent <- match.arg(tolower(adjacent)[1],
+                         choices = c(
+                           "parents",
+                           "mothers",
+                           "fathers"
+                         )
+  )
+  # Check the needed IDs are in the data
+  if(adjacent == 'parents'){
+    needIds <- c(personID, momID, dadID)
+  } else if(adjacent == 'mothers'){
+    needIds <- c(personID, momID)
+  } else if(adjacent == 'fathers') {
+    needIds <- c(personID, dadID)
+  }
+  if (!all(c(needIds) %in% names(ped))){
+    msg <- paste0("The following ID variables are needed but were not found:\n",
+                  paste(needIds[!(c(needIds) %in% names(ped))], collapse=', '),
+                  "\n",
+                  "Make sure you have the variable names correct.")
+    stop(msg)
+  }
 
-  nodes <- unique(
-    stats::na.omit(
-      as.character(c(ped[[personID]], ped[[momID]], ped[[dadID]]))
+  # Create nodes and edges
+  if(adjacent == 'parents'){
+    nodes <- unique(
+      stats::na.omit(
+        as.character(c(ped[[personID]], ped[[momID]], ped[[dadID]]))
+      )
     )
-  )
-  edges <- rbind(
-    as.matrix(sapply(ped[, c(personID, momID)], as.character)),
-    as.matrix(sapply(ped[, c(personID, dadID)], as.character))
-  )
+    edges <- rbind(
+      as.matrix(sapply(ped[, c(personID, momID)], as.character)),
+      as.matrix(sapply(ped[, c(personID, dadID)], as.character))
+    )
+  } else if(adjacent == 'mothers'){
+    nodes <- unique(
+      stats::na.omit(
+        as.character(c(ped[[personID]], ped[[momID]]))
+      )
+    )
+    edges <- as.matrix(sapply(ped[, c(personID, momID)], as.character))
+  } else if(adjacent == 'fathers') {
+    nodes <- unique(
+      stats::na.omit(
+        as.character(c(ped[[personID]], ped[[dadID]]))
+      )
+    )
+    edges <- as.matrix(sapply(ped[, c(personID, dadID)], as.character))
+  }
   edges <- edges[stats::complete.cases(edges), ]
 
   # Make graph
@@ -85,6 +140,7 @@ ped2graph <- function(ped,
 
 #' Add a maternal line ID variable to a pedigree
 #' @inheritParams ped2fam
+#' @param matID Character.  Maternal line ID variable to be created and added to the pedigree
 #' @details
 #' Under various scenarios it is useful to know which people in a pedigree
 #' belong to the same maternal lines.  This function first turns a pedigree
@@ -95,10 +151,14 @@ ped2graph <- function(ped,
 #' for creating paternal line IDs
 #' @export
 #'
-ped2maternal <- function(ped, personID = "ID", momID = "momID", dadID = "dadID", famID = "famID"){}
+ped2maternal <- function(ped, personID = "ID", momID = "momID", dadID = "dadID", matID = "matID"){
+  # Call to wrapper function
+  .ped2id(ped = ped, personID = personID, momID = momID, dadID = dadID, famID = matID, type = 'mothers')
+}
 
 #' Add a paternal line ID variable to a pedigree
 #' @inheritParams ped2fam
+#' @param patID Character.  Paternal line ID variable to be created and added to the pedigree
 #' @details
 #' Under various scenarios it is useful to know which people in a pedigree
 #' belong to the same paternal lines.  This function first turns a pedigree
@@ -109,4 +169,7 @@ ped2maternal <- function(ped, personID = "ID", momID = "momID", dadID = "dadID",
 #' for creating maternal line IDs
 #' @export
 #'
-ped2paternal <- function(ped, personID = "ID", momID = "momID", dadID = "dadID", famID = "famID"){}
+ped2paternal <- function(ped, personID = "ID", momID = "momID", dadID = "dadID", patID = "patID"){
+  # Call to wrapper function
+  .ped2id(ped = ped, personID = personID, momID = momID, dadID = dadID, famID = patID, type = 'fathers')
+}
