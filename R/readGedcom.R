48db447507ee6380c7af6802c3d48813e406cf03
Mason Garrison <garrissm@wfu.edu>
2025-04-07 18:38:40 -0400

    refactoring

--- R/readGedcom.R
+++ R/readGedcom.R
@@ -120,38 +120,34 @@ readGedcom <- function(file_path,
       next
     }
     # PERSONAL_NAME_PIECES := NAME | NPFX | GIVN | NICK | SPFX | SURN | NSFX
-    if (num_rows$num_givn_rows > 0 && grepl(" GIVN", tmpv)) {
-      vars$name_given_pieces <- extract_info(tmpv, "GIVN")
-      next
-    }
+    result <- process_tag("GIVN", "name_given_pieces", num_rows, tmpv, vars)
+    vars <- result$vars
+    if (result$matched) next
 
     # npfx := Name Prefix
-    if (num_rows$num_npfx_rows > 0 && grepl(" NPFX", tmpv)) {
-      vars$name_npfx <- extract_info(tmpv, "NPFX")
-      next
-    }
+    result <- process_tag("NPFX", "name_npfx", num_rows, tmpv, vars)
+    vars <- result$vars
+    if (result$matched) next
 
     # NICK := Nickname
-    if (num_rows$num_nick_rows > 0 && grepl(" NICK", tmpv)) {
-      vars$name_nick <- extract_info(tmpv, "NICK")
-      next
-    }
+    result <- process_tag("NICK", "name_nick", num_rows, tmpv, vars)
+    vars <- result$vars
+    if (result$matched) next
 
     # surn := Surname
-    if (num_rows$num_surn_rows > 0 && grepl(" SURN", tmpv)) {
-      vars$name_surn_pieces <- extract_info(tmpv, "SURN")
-      next
-    }
+    result <- process_tag("SURN", "name_surn_pieces", num_rows, tmpv, vars)
+    vars <- result$vars
+    if (result$matched) next
 
     # nsfx := Name suffix
-    if (num_rows$num_nsfx_rows > 0 && grepl(" NSFX", tmpv)) {
-      vars$name_nsfx <- extract_info(tmpv, "NSFX")
-      next
-    }
-    if (num_rows$num_marnm_rows > 0 && grepl(" _MARNM", tmpv)) {
-      vars$name_marriedsurn <- extract_info(tmpv, "_MARNM")
-      next
-    }
+    result <- process_tag("NSFX", "name_nsfx", num_rows, tmpv, vars)
+    vars <- result$vars
+    if (result$matched) next
+
+    result <- process_tag("_MARNM", "name_marriedsurn", num_rows, tmpv, vars)
+    vars <- result$vars
+    if (result$matched) next
+
     # Birth event related information
     if (num_rows$num_birt_rows > 0 && grepl(" BIRT", tmpv)) {
       if (num_rows$num_date_rows > 0 && i + 1 <= file_length) {
@@ -190,118 +186,85 @@ readGedcom <- function(file_path,
       next
     }
 
-    if (grepl(" SEX", tmpv)) {
-      vars$sex <- extract_info(tmpv, "SEX")
-      next
-    }
 
-    # Individual Attributes
+    # Attribute tags using process_tag()
+    for (tag_field in list(
+      c("SEX",  "sex"),
 
-    # CAST	caste
-    #  g7:CAST	The name of an individual’s rank or status in society which is sometimes based on racial or religious differences, or differences in wealth, inherited rank, profession, or occupation.
-    if (num_rows$num_cast_rows > 0 && grepl(" CAST", tmpv)) {
-      vars$attribute_caste <- extract_info(tmpv, "CAST")
-      next
-    }
-    # DSCR	physical description
-    # g7:DSCR	The physical characteristics of a person.
-    if (num_rows$num_dscr_rows > 0 && grepl(" DSCR", tmpv)) {
-      vars$attribute_description <- extract_info(tmpv, "DSCR")
-      next
-    }
-    # EDUC	education
-    #  g7:EDUC	Indicator of a level of education attained.
-    if (num_rows$num_educ_rows > 0 && grepl(" EDUC", tmpv)) {
-      vars$attribute_education <- extract_info(tmpv, "EDUC")
-      next
-    }
-    # IDNO	identifying number
-    # g7:IDNO	A number or other string assigned to identify a person within some significant external system. It must have a TYPE substructure to define what kind of identification number is being provided.
-    if (num_rows$num_idno_rows > 0 && grepl(" IDNO", tmpv)) {
-      vars$attribute_idnumber <- extract_info(tmpv, "IDNO")
-      next
-    }
-    # NATI	nationality
-    # g7:NATI	An individual’s national heritage or origin, or other folk, house, kindred, lineage, or tribal interest.
-    if (num_rows$num_nati_rows > 0 && grepl(" NATI", tmpv)) {
-      vars$attribute_nationality <- extract_info(tmpv, "NATI")
-      next
-    }
-    # NCHI	number of children
-    # g7:INDI-NCHI	The number of children that this person is known to be the parent of (all marriages).
-    if (num_rows$num_nchi_rows > 0 && grepl(" NCHI", tmpv)) {
-      vars$attribute_children <- extract_info(tmpv, "NCHI")
-      next
-    }
+      # CAST	caste
+      #  g7:CAST	The name of an individual’s rank or status in society which is sometimes based on racial or religious differences, or differences in wealth, inherited rank, profession, or occupation.
+      c("CAST", "attribute_caste"),
 
-    # NMR	number of marriages
-    # g7:NMR	The number of times this person has participated in a family as a spouse or parent.
-    if (num_rows$num_nmr_rows > 0 && grepl(" NMR", tmpv)) {
-      vars$attribute_marriages <- extract_info(tmpv, "NMR")
-      next
-    }
+      # DSCR	physical description
+      # g7:DSCR	The physical characteristics of a person.
+      c("DSCR", "attribute_description"),
 
-    # OCCU	occupation
-    # g7:OCCU	The type of work or profession of an individual.
-    if (num_rows$num_occu_rows > 0 && grepl(" OCCU", tmpv)) {
-      vars$attribute_occupation <- extract_info(tmpv, "OCCU")
-      next
-    }
-    # PROP	property
-    # g7:PROP	Pertaining to possessions such as real estate or other property of interest.
+      # EDUC	education
+      #  g7:EDUC	Indicator of a level of education attained.
+      c("EDUC", "attribute_education"),
 
-    if (num_rows$num_prop_rows > 0 && grepl(" PROP", tmpv)) {
-      vars$attribute_property <- extract_info(tmpv, "PROP")
-      next
-    }
+      # IDNO	identifying number
+      # g7:IDNO	A number or other string assigned to identify a person within some significant external system. It must have a TYPE substructure to define what kind of identification number is being provided.
+      c("IDNO", "attribute_idnumber"),
 
-    # RELI	religion
-    #  g7:INDI-RELI	A religious denomination to which a person is affiliated or for which a record applies.
-    if (num_rows$num_reli_rows > 0 && grepl(" RELI", tmpv)) {
-      vars$attribute_religion <- extract_info(tmpv, "RELI")
-      next
-    }
-    # RESI	residence
-    # g7:INDI-RESI	An address or place of residence where an individual resided.
-    if (num_rows$num_resi_rows > 0 && grepl(" RESI", tmpv)) {
-      vars$attribute_residence <- extract_info(tmpv, "RESI")
-      next
-    }
+      # NATI	nationality
+      # g7:NATI	An individual’s national heritage or origin, or other folk, house, kindred, lineage, or tribal interest.
+      c("NATI", "attribute_nationality"),
 
-    # SSN	social security number
-    # g7:SSN	A number assigned by the United States Social Security Administration, used for tax identification purposes. It is a type of IDNO.
-    if (num_rows$num_ssn_rows > 0 && grepl(" SSN", tmpv)) {
-      vars$attribute_ssn <- extract_info(tmpv, "SSN")
-      next
-    }
-    # TITL	title
-    # g7:INDI-TITL	A formal designation used by an individual in connection with positions of royalty or other social status, such as Grand Duke.
-    if (num_rows$num_titl_rows > 0 && grepl(" TITL", tmpv)) {
-      vars$attribute_title <- extract_info(tmpv, "TITL")
-      next
+      # NCHI	number of children
+      # g7:INDI-NCHI	The number of children that this person is known to be the parent of (all marriages).
+      c("NCHI", "attribute_children"),
+
+      # NMR	number of marriages
+      # g7:NMR	The number of times this person has participated in a family as a spouse or parent.
+      c("NMR",  "attribute_marriages"),
+
+      # OCCU	occupation
+      # g7:OCCU	The type of work or profession of an individual.
+      c("OCCU", "attribute_occupation"),
+
+      # PROP	property
+      # g7:PROP	Pertaining to possessions such as real estate or other property of interest.
+      c("PROP", "attribute_property"),
+
+      # RELI	religion
+      #  g7:INDI-RELI	A religious denomination to which a person is affiliated or for which a record applies.
+      c("RELI", "attribute_religion"),
+
+      # RESI	residence
+      # g7:INDI-RESI	An address or place of residence where an individual resided.
+      c("RESI", "attribute_residence"),
+
+      # SSN	social security number
+      # g7:SSN	A number assigned by the United States Social Security Administration, used for tax identification purposes. It is a type of IDNO.
+      c("SSN",  "attribute_ssn"),
+
+      # TITL	title
+      # g7:INDI-TITL	A formal designation used by an individual in connection with positions of royalty or other social status, such as Grand Duke.
+      c("TITL", "attribute_title")
+    )) {
+      result <- process_tag(tag_field[1], tag_field[2], num_rows, tmpv, vars)
+      vars <- result$vars
+      if (result$matched) next
     }
 
     # relationship data
     # g7:INDI-FAMC
     ## The family in which an individual appears as a child. It is also used with a g7:FAMC-STAT substructure to show individuals who are not children of the family. See FAMILY_RECORD for more details.
-    if (num_rows$num_famc_rows > 0 && grepl(" FAMC", tmpv)) {
-      if (is.na(vars$FAMC)) {
-        vars$FAMC <- stringr::str_extract(tmpv, "(?<=@.)\\d*(?=@)")
-      } else {
-        vars$FAMC <- paste0(vars$FAMC, ", ", stringr::str_extract(tmpv, "(?<=@.)\\d*(?=@)"))
-      }
-      next
-    }
+result <- process_tag("FAMC", "FAMC", num_rows, tmpv, vars,
+                      extractor = function(x) stringr::str_extract(x, "(?<=@.)\\d*(?=@)"),
+                      mode = "append")
+    vars <- result$vars
+    if (result$matched) next
+
     # FAMS (Family spouse) g7:FAMS
     #  The family in which an individual appears as a partner. See FAMILY_RECORD for more details.
-    if (num_rows$num_fams_rows > 0 && grepl(" FAMS", tmpv)) {
-      if (is.na(vars$FAMS)) {
-        vars$FAMS <- stringr::str_extract(tmpv, "(?<=@.)\\d*(?=@)")
-      } else {
-        vars$FAMS <- paste0(vars$FAMS, ", ", stringr::str_extract(tmpv, "(?<=@.)\\d*(?=@)"))
-      }
-      next
-    }
+    result <- process_tag("FAMS", "FAMS", num_rows, tmpv, vars,
+                          extractor = function(x) stringr::str_extract(x, "(?<=@.)\\d*(?=@)"),
+                          mode = "append")
+    vars <- result$vars
+    if (result$matched) next
+
     if (verbose && i %% 1000 == 0) {
       cat("Processed", i, "lines\n")
     }
@@ -383,8 +346,7 @@ readGedcom <- function(file_path,
 #' @param df_temp A data frame containing information about individuals.
 #' @return A list mapping family IDs to parent IDs.
 #' @keywords internal
-createFamilyToParentsMapping <- function(df_temp, datasource) {
-  if (datasource == "gedcom") {
+mapFAMS2parents <- function(df_temp) {
     if (!all(c("FAMS", "sex") %in% colnames(df_temp))) {
       warning("The data frame does not contain the necessary columns (FAMS, sex)")
       return(NULL)
@@ -411,10 +373,6 @@ createFamilyToParentsMapping <- function(df_temp, datasource) {
         }
       }
     }
-  } else if (datasource == "wiki") {
-    message("The data source is not supported")
-    return(df_temp)
-  }
   return(family_to_parents)
 }
 
@@ -425,13 +383,11 @@ createFamilyToParentsMapping <- function(df_temp, datasource) {
 #'
 #' @param df_temp A data frame containing individual information.
 #' @param family_to_parents A list mapping family IDs to parent IDs.
-#' @param datasource A string indicating the data source. Options are "gedcom" and "wiki".
 #' @return A data frame with added momID and dad_ID columns.
 #' @keywords internal
-assignParentIDs <- function(df_temp, family_to_parents, datasource) {
+mapFAMC2parents <- function(df_temp, family_to_parents) {
   df_temp$momID <- NA_character_
   df_temp$dadID <- NA_character_
-  if (datasource == "gedcom") {
     for (i in 1:nrow(df_temp)) {
       if (!is.na(df_temp$FAMC[i])) {
         famc_ids <- unlist(strsplit(df_temp$FAMC[i], ", "))
@@ -448,10 +404,6 @@ assignParentIDs <- function(df_temp, family_to_parents, datasource) {
       }
     }
     return(df_temp)
-  } else if (datasource == "wiki") {
-    message("No parents information available for wiki data")
-    return(df_temp)
-  }
 }
 
 #' Process parents information
@@ -477,11 +429,11 @@ processParents <- function(df_temp, datasource) {
     return(df_temp)
   }
 
-  family_to_parents <- createFamilyToParentsMapping(df_temp, datasource = datasource)
+  family_to_parents <- mapFAMS2parents(df_temp)
   if (is.null(family_to_parents) || length(family_to_parents) == 0) {
     return(df_temp)
   }
-  df_temp <- assignParentIDs(df_temp, family_to_parents, datasource = datasource)
+  df_temp <- mapFAMC2parents(df_temp, family_to_parents)
   return(df_temp)
 }
 
@@ -574,5 +526,38 @@ countPatternRows <- function(file) {
     num_caus_rows = pattern_counts[" CAUS"]
   )
   return(num_rows)
+}
+
+#' Process a GEDCOM Tag
+#'
+#' Extracts and assigns a value to a specified field in `vars` if the pattern is present.
+#' Returns both the updated variable list and a flag indicating whether the tag was matched.
+#'
+#' @param tag The GEDCOM tag (e.g., "SEX", "CAST", etc.).
+#' @param field_name The name of the variable to assign to in `vars`.
+#' @param pattern_rows Output from `countPatternRows()`.
+#' @param line The GEDCOM line to parse.
+#' @param vars The current list of variables to update.
+#' @return A list with updated `vars` and a `matched` flag.
+#' @keywords internal
+process_tag <- function(tag, field_name, pattern_rows, line, vars,
+                        extractor = NULL, mode = "replace") {
+  count_name <- paste0("num_", tolower(tag), "_rows")
+  matched <- FALSE
+  if (!is.null(pattern_rows[[count_name]]) &&
+      pattern_rows[[count_name]] > 0 &&
+      grepl(paste0(" ", tag), line)) {
+
+    value <- if (is.null(extractor)) extract_info(line, tag) else extractor(line)
+
+    if (mode == "append" && !is.na(vars[[field_name]])) {
+      vars[[field_name]] <- paste0(vars[[field_name]], ", ", value)
+    } else {
+      vars[[field_name]] <- value
+    }
+
+    matched <- TRUE
+  }
+  return(list(vars = vars, matched = matched))
 }
 
