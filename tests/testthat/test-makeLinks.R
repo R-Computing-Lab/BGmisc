c5eabef3ccce44d39580740a9a6799ea8ceced08
Mason Garrison <garrissm@wfu.edu>
2025-04-10 10:58:00 -0400

    splitting links into legacy

--- tests/testthat/test-makeLinks.R
+++ tests/testthat/test-makeLinks.R
@@ -7,10 +7,9 @@ test_that("com2links handles missing matrices properly", {
 
 
 
-
 test_that("com2links rejects invalid matrix types", {
   fake_matrix <- data.frame(A = c(1, 2), B = c(3, 4))
-  expect_error(com2links(ad_ped_matrix = fake_matrix), "The 'ad_ped_matrix' must be a matrix or dgCMatrix.")
+  expect_error(com2links(ad_ped_matrix = fake_matrix), "The 'ad_ped_matrix' must be a matrix or generalMatrix")
 })
 
 test_that("com2links produces correct output with a single relationship matrix (hazard dataset)", {
@@ -27,12 +26,12 @@ test_that("com2links produces correct output with a single relationship matrix (
 
 test_that("com2links produces correct output with cn_ped_matrix", {
   data(ASOIAF)
-  cn_ped_matrix <- ped2mit(ASOIAF, sparse = TRUE)
+  cn_ped_matrix <- ped2cn(ASOIAF, sparse = TRUE)
 
   result <- com2links(cn_ped_matrix = cn_ped_matrix, writetodisk = FALSE)
 
   expect_true(is.data.frame(result))
-  expect_true(all(c("ID1", "ID2", "cnRel") %in% colnames(result)))
+  expect_true(all(c("ID1", "ID2", "cnuRel") %in% colnames(result)))
   expect_equal(ncol(result), 3) # Expect ID1, ID2, and addRel
   expect_true(all(result$cnRel >= 0)) # Relatedness values should be non-negative
 })
@@ -65,7 +64,47 @@ test_that("com2links processes multiple matrices correctly (hazard dataset)", {
   expect_true(all(result$cnuRel >= 0))
 })
 
+test_that("com2links processes creates same length for cn with 3, 2, and 1 matrices are used", {
+  data(hazard)
+  ad_ped_matrix <- ped2add(hazard, sparse = TRUE)
+  mit_ped_matrix <- ped2mit(hazard, sparse = TRUE)
+  cn_ped_matrix <- ped2cn(hazard, sparse = TRUE)
 
+  result3 <- com2links(ad_ped_matrix = ad_ped_matrix, mit_ped_matrix = mit_ped_matrix, cn_ped_matrix = cn_ped_matrix, writetodisk = FALSE)
+
+  expect_true(is.data.frame(result3))
+  expect_true(all(c("ID1", "ID2", "addRel", "mitRel", "cnuRel") %in% colnames(result3)))
+  expect_equal(ncol(result3), 5) # Expect ID1, ID2, addRel, mitRel, and cnuRel
+  expect_true(all(result3$addRel >= 0))
+  expect_true(all(result3$mitRel %in% c(0, 1))) # Mitochondrial should be binary
+  expect_true(all(result3$cnuRel >= 0))
+
+  result2 <- com2links(ad_ped_matrix = ad_ped_matrix, cn_ped_matrix = cn_ped_matrix, writetodisk = FALSE)
+  expect_true(is.data.frame(result2))
+  expect_true(all(c("ID1", "ID2", "addRel", "cnuRel") %in% colnames(result2)))
+  expect_equal(ncol(result2), 4) # Expect ID1, ID2, addRel, and cnuRel
+  expect_true(all(result2$addRel >= 0))
+  expect_true(all(result2$cnuRel >= 0))
+
+  expect_equal(result3$cnuRel,result2$cnuRel)
+
+  result1 <- com2links(cn_ped_matrix = cn_ped_matrix, writetodisk = FALSE)
+  result1_legacy <- com2links.legacy(cn_ped_matrix = cn_ped_matrix, writetodisk = FALSE)
+  expect_true(is.data.frame(result1))
+  expect_true(is.data.frame(result1_legacy))
+  expect_true(all(c("ID1", "ID2", "cnuRel") %in% colnames(result1)))
+  expect_true(all(c("ID1", "ID2", "cnuRel") %in% colnames(result1_legacy)))
+  expect_equal(ncol(result1), 3) # Expect ID1, ID2, and cnuRel
+  expect_equal(ncol(result1_legacy), 3) # Expect ID1, ID2, and cnuRel
+  expect_true(all(result1$cnuRel >= 0))
+  expect_true(all(result1_legacy$cnuRel >= 0))
+  expect_equal(result3$cnuRel,result1$cnuRel)
+  expect_equal(result3$cnuRel,result1_legacy$cnuRel)
+  expect_equal(result2$cnuRel,result1$cnuRel)
+  expect_equal(result2$cnuRel,result1_legacy$cnuRel)
+  expect_equal(result1$cnuRel,result1_legacy$cnuRel)
+
+})
 test_that("com2links written version matchs", {
   data(hazard)
   ad_ped_matrix <- ped2com(hazard, component = "additive", adjacency_method = "direct", sparse = TRUE)
@@ -236,7 +275,7 @@ test_that("com2links correctly handles missing matrices", {
     "At least one of 'ad_ped_matrix', 'mit_ped_matrix', or 'cn_ped_matrix' must be provided."
   )
 
-  expect_error(com2links(ad_ped_matrix = hazard), "The 'ad_ped_matrix' must be a matrix or dgCMatrix.")
+  expect_error(com2links(ad_ped_matrix = hazard), "The 'ad_ped_matrix' must be a matrix or generalMatrix")
 })
 
 test_that("com2links correctly processes inbreeding dataset", {
@@ -281,14 +320,16 @@ test_that("com2links handles large batch writing correctly", {
   sexR <- 0.5
   df_fam <- simulatePedigree(kpc = kpc, Ngen = Ngen, sexR = sexR, marR = marR)
 
-  ad_ped_matrix <- ped2add(df_fam, sparse = TRUE)
+  cn_ped_matrix <- ped2cn(df_fam, sparse = TRUE)
 
   temp_file <- tempfile(fileext = ".csv")
-  com2links(ad_ped_matrix = ad_ped_matrix, rel_pairs_file = temp_file, writetodisk = TRUE, verbose = TRUE)
+  com2links(cn_ped_matrix = cn_ped_matrix, rel_pairs_file = temp_file, writetodisk = TRUE, verbose = TRUE)
 
   expect_true(file.exists(temp_file))
   written_data <- read.csv(temp_file)
   expect_true(nrow(written_data) > 1000) # Ensuring batch writing logic works
+  expect_true(file.remove(temp_file))
+
 })
 
 test_that("com2links garbage collection does not affect output, using two components", {
