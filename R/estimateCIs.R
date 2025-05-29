5fee4f9edcd8f32d7c6b513241bcbdaa2702bd5e
Mason Garrison <garrissm@wfu.edu>
2025-05-29 14:47:06 -0400

    overhauled cis

--- R/estimateCIs.R
+++ R/estimateCIs.R
@@ -5,20 +5,26 @@
 #' @param tbl A data frame or tibble containing the correlation coefficient and standard error variables.
 #' @param rho_var The name of the column in \code{tbl} that contains the correlation coefficients.
 #' @param se_var The name of the column in \code{tbl} that contains the standard errors.
+#' @param doubleentered Logical. If \code{TRUE}, the function assumes that the correlation coefficients are double-entered, which adjusts the standard errors accordingly. Default is \code{FALSE}.
 #' @param method The method to use for calculating the confidence intervals. Options are "raykov", "fisherz", "doubleenteredconserv", or "doubleentered".
 #' @param conf_level The confidence level for the intervals. Default is 0.95.
-#'
+#' @param adjust_base A numeric value to adjust the standard errors. Default is 1.
 #' @return A modified version of \code{tbl} with additional columns for the confidence intervals and related statistics.
-#'
+#' Everything uses adjusted standard errors, including confidence intervals, z-tests, and p-values.
+
 #' @examples
 #' tbl <- data.frame(rho = c(0.5, 0.7, 0.3), se = c(0.1, 0.2, 0.05))
 #' calculateCIs(tbl, rho_var = "rho", se_var = "se", method = "raykov")
 #'
 #' @export
-calculateCIs <- function(tbl, rho_var, se_var, method = "raykov", conf_level = 0.95) {
+calculateCIs <- function(tbl,
+                         rho_var,
+                         se_var,
+                         doubleentered = FALSE,
+                         method = "raykov",
+                         adjust_base = 1,
+                         conf_level = 0.95) {
   # Load necessary packages
-  require(rlang)
-
 
   # Get the name of the rho column regardless of input type
  if(mode(rho_var) != "character") {
@@ -32,42 +38,62 @@ calculateCIs <- function(tbl, rho_var, se_var, method = "raykov", conf_level = 0
   plusse_col_name <- paste0(rho_col_name, "_plusse")
   minusse_col_name <- paste0(rho_col_name, "_minusse")
   z_col_name <- paste0(rho_col_name, "_z")
+  wald_col_name <- paste0(rho_col_name, "_wald")
   ztest_col_name <- paste0(rho_col_name, "_ztest")
-  p1tail_col_name <- paste0(rho_col_name, "_p1tail")
-  p2tail_col_name <- paste0(rho_col_name, "_p2tail")
+  zp1tail_col_name <- paste0(rho_col_name, "_zp1tail")
+  zp2tail_col_name <- paste0(rho_col_name, "_zp2tail")
   sez_col_name <- paste0(se_col_name, "_sez")
+  se_adjusted_col_name <- paste0(se_col_name,"_se_adjusted")
+  sez_adjusted_col_name <- paste0(se_col_name,"_sez_adjusted")
+  waldp1tail_col_name <- paste0(rho_col_name, "_waldp1tail")
+  waldp2tail_col_name <- paste0(rho_col_name, "_waldp2tail")
 
   # Create a copy of tbl to avoid modifying the original data frame
   tbl_out <- tbl
 
+  if(doubleentered==TRUE){
+    adjust <- sqrt(2)
+  } else if(doubleentered==FALSE){
+    adjust <- adjust_base
+  }
+  ci_b <- adjust * qnorm((1 + conf_level)/2)
+
   if (method == "raykov") {
     # Apply Fisher's r to z transform
     z_vals <- psych::fisherz(tbl_out[[rho_col_name]])
-    se_vals <- tbl_out[[se_col_name]] / (1 - tbl_out[[rho_col_name]]^2)
+    sez_vals <- tbl_out[[se_col_name]] / (1 - tbl_out[[rho_col_name]]^2)
 
     tbl_out[[z_col_name]] <- z_vals
-    tbl_out[[sez_col_name]] <- se_vals
-    tbl_out[[plusse_col_name]] <- psych::fisherz2r(z_vals + qnorm((1 + conf_level) / 2) * se_vals)
-    tbl_out[[minusse_col_name]] <- psych::fisherz2r(z_vals - qnorm((1 + conf_level) / 2) * se_vals)
+    tbl_out[[sez_col_name]] <- sez_vals
+    tbl_out[[se_adjusted_col_name]] <- tbl_out[[se_col_name]] * adjust
+    tbl_out[[sez_adjusted_col_name]] <- tbl_out[[se_adjusted_col_name]] / (1 - tbl_out[[rho_col_name]]^2)
+    tbl_out[[plusse_col_name]] <- psych::fisherz2r(z_vals + ci_b * sez_vals)
+    tbl_out[[minusse_col_name]] <- psych::fisherz2r(z_vals - ci_b * sez_vals)
 
     # H0: r = 0. Compute z-test and p-values
-    ztest_vals <- tbl_out[[rho_col_name]] / tbl_out[[se_col_name]]
+    ztest_vals <- tbl_out[[z_col_name]] / tbl_out[[sez_adjusted_col_name]]
+
     tbl_out[[ztest_col_name]] <- ztest_vals
-    tbl_out[[p1tail_col_name]] <- pnorm(q = abs(ztest_vals), lower.tail = FALSE)
-    tbl_out[[p2tail_col_name]] <- 2 * tbl_out[[p1tail_col_name]]
+    tbl_out[[zp1tail_col_name]] <- pnorm(q = abs(ztest_vals), lower.tail = FALSE)
+    tbl_out[[zp2tail_col_name]] <- 2 * tbl_out[[zp1tail_col_name]]
 
   } else {
     # Set the multiplier for CI calculations based on the method
-    ci_multi <- switch(method,
-                       "fisherz" = 1.96,  # 95% CI, can generalize to other levels
-                       "doubleenteredconserv" = sqrt(2) * 1.96,
-                       "doubleentered" = sqrt(2),
-                       stop("method must be one of 'fisherz', 'doubleenteredconserv', or 'doubleentered'"))
+#    ci_multi <- switch(method,
+#                       "fisherz" = 1.96,  # 95% CI, can generalize to other levels
+#                       "doubleenteredconserv" = sqrt(2) * 1.96,
+#                       "doubleentered" = sqrt(2),
+#                       stop("method must be one of 'fisherz', 'doubleenteredconserv', or 'doubleentered'"))
 
     # Compute confidence intervals
-    tbl_out[[plusse_col_name]] <- tbl_out[[rho_col_name]] + ci_multi * tbl_out[[se_col_name]]
-    tbl_out[[minusse_col_name]] <- tbl_out[[rho_col_name]] - ci_multi * tbl_out[[se_col_name]]
+    tbl_out[[plusse_col_name]] <- tbl_out[[rho_col_name]] + ci_b * tbl_out[[se_col_name]]
+    tbl_out[[minusse_col_name]] <- tbl_out[[rho_col_name]] - ci_b * tbl_out[[se_col_name]]
   }
+  # Compute Wald statistics
+  tbl_out[[wald_col_name]] <- tbl_out[[rho_col_name]] / tbl_out[[se_adjusted_col_name]]
+  tbl_out[[waldp1tail_col_name]] <- pnorm(q = abs(tbl_out[[wald_col_name]]), lower.tail = FALSE)
+  tbl_out[[waldp2tail_col_name]] <- 2 * tbl_out[[waldp1tail_col_name]]
+
 
   return(tbl_out)
 }
