a846db69539705a866ea04aaabe0660465aa33e5
Mason Garrison <garrissm@wfu.edu>
2025-05-29 11:58:22 -0400

    Update test-buildComponent.R

--- tests/testthat/test-buildComponent.R
+++ tests/testthat/test-buildComponent.R
@@ -1,3 +1,13 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that(".assignParentValue works", {
+
+  expect_equal(.assignParentValue("generation"), .5)
+  expect_equal(.assignParentValue("additive"), .5)
+  expect_equal(.assignParentValue("common nuclear"), 1)
+  expect_equal(.assignParentValue("mitochondrial"), 1)
+  expect_equal(.assignParentValue("mtdna"), 1)
+  expect_equal(.assignParentValue("mitochondria"), 1)
+
+  expect_error(.assignParentValue("unknown component"),
+               "Don't know how to set parental value")
 })
+
