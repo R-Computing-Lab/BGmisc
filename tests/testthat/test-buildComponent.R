ae3d361c4e9c9879aafe5534861abd3ef44cc0af
Mason Garrison <6001608+smasongarrison@users.noreply.github.com>
2025-10-14 11:46:36 -0400

    Add ped2gen function and corresponding tests
    Introduced the ped2gen function to extract generation information from pedigree data.frames. Added comprehensive tests for ped2gen to validate generation assignment and ensure correct behavior for founders and their descendants.

--- tests/testthat/test-buildComponent.R
+++ tests/testthat/test-buildComponent.R
@@ -11,3 +11,41 @@ test_that(".assignParentValue works", {
     "Don't know how to set parental value"
   )
 })
+
+
+test_that("ped2gen produces correct correct generations for hazzard and
+deviantions all make sense", {
+  data(hazard)
+  df_hazard <- hazard
+  df_hazard$gen_og <- df_hazard$gen
+
+  # Recalculate generations
+  df_hazard$gen <- ped2gen(df_hazard, sparse = FALSE)
+
+  expect_true(is.numeric(df_hazard$gen))
+
+  # all differences should be 0 except for founders because the og data uses spouses to set generation for founders
+
+  df_hazard$gen_diff <- abs(df_hazard$gen_og - df_hazard$gen)
+  expect_true(all(df_hazard$gen[is.na(df_hazard$momID) & is.na(df_hazard$dadID)] == 1)) # Founders all be 1
+  expect_true(all(df_hazard$gen_diff[!(is.na(df_hazard$momID) & is.na(df_hazard$dadID))] == 0)) # Non-founders match original
+
+  # check min generation for children of founders
+  founders <- df_hazard$ID[is.na(df_hazard$momID) & is.na(df_hazard$dadID)]
+  expect_true(all(df_hazard$gen[df_hazard$momID %in% founders | df_hazard$dadID %in% founders] > 1)) # Children of founders > 1
+
+  # get min generation for children of founders by founder
+  for (f in founders) {
+    children <- df_hazard$ID[df_hazard$momID == f | df_hazard$dadID == f]
+    if (length(children) > 0) {
+      df_hazard$min_gen_children[df_hazard$ID == f] <- min(df_hazard$gen[df_hazard$ID %in% children])
+    }
+  }
+
+  expect_true(all(df_hazard$min_gen_children[df_hazard$ID %in% founders] > 1)) # Children of founders should be generation 2
+  expect_true(all(is.na(df_hazard$min_gen_children[!df_hazard$ID %in% founders])))
+
+
+  # reconstruct the orginal generation values from generated values of children
+  expect_true(all(df_hazard$min_gen_children[df_hazard$ID %in% founders] - 1 == df_hazard$gen_og[df_hazard$ID %in% founders]))
+})
