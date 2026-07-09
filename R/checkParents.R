f62ffc622a4280da3e551427d50585a32b19cc4b
Mason Garrison <6001608+smasongarrison@users.noreply.github.com>
2026-07-09 10:02:13 -0400

    fix check id error

--- R/checkParents.R
+++ R/checkParents.R
@@ -110,16 +110,18 @@ checkParentIDs <- function(ped, verbose = FALSE, repair = FALSE,
   }
   # Are any parents in both momID and dadID?
   momdad <- intersect(ped$dadID, ped$momID)
-  if (any(length(momdad) > 0 && !is.na(momdad))) {
-    validation_results$parents_in_both <- momdad
-    if (verbose == TRUE) {
-      cat(paste(
-        "Some individuals appear in both momID and dadID roles.\n",
-        "These individuals are:\n"
-      ))
-      message(momdad)
-    }
+  momdad <- momdad[!is.na(momdad)]
+if (length(momdad) > 0) {
+  validation_results$parents_in_both <- momdad
+  if (verbose == TRUE) {
+    cat(
+      "Some individuals appear in both momID and dadID roles.\n",
+      "These individuals are:\n",
+      sep = ""
+    )
+    message(paste(momdad, collapse = ", "))
   }
+}
 
 
   if (!repair) {
